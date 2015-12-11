fb7619832cc1abbb9f164905bdf11dde061313a6
Mitch D'Ewart <mdewart@slac.stanford.edu>
2015-12-11 13:36:11 -0800

    removed unnecessary substitution files...

---
+++ cpswAsynPortDriverApp/Db/O.linux-x86/testAsynPortDriver.db.d
@@ -0,0 +1,8 @@
+# DO NOT EDIT: This file created by mkmf.pl,v 1.5 2002/03/25 21:33:24 jba Exp $ 
+
+../O.Common/testAsynPortDriver.db : ../testAsynPortDriver.template
+../O.Common/testAsynPortDriver.db : ../testAsynPortDriverWaveform.template
+
+
+../O.Common/testAsynPortDriver.db : ../testAsynPortDriver.template
+testAsynPortDriver.db.d: ../testAsynPortDriver.template
