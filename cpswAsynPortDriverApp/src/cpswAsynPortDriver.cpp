459653fe169a1ff765fc143ddcfe9e9bc22846d7
Mitch D'Ewart <mdewart@slac.stanford.edu>
2015-12-11 14:41:08 -0800

    added cpswAsynPortDriverApp/src

---
+++ cpswAsynPortDriverApp/src/cpswAsynPortDriver.cpp
@@ -0,0 +1,454 @@
+/*
+ * cpswAsynDriver.cpp
+ * 
+ * Asyn driver that inherits from the asynPortDriver class to demonstrate its use.
+ * It simulates a digital scope looking at a 1kHz 1000-point noisy sine wave.  Controls are
+ * provided for time/division, volts/division, volt offset, trigger delay, noise amplitude, update time,
+ * and run/stop.
+ * Readbacks are provides for the waveform data, min, max and mean values.
+ *
+ * Author: Mark Rivers
+ *
+ * Created Feb. 5, 2009
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <math.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+#include <epicsTypes.h>
+#include <epicsTime.h>
+#include <epicsThread.h>
+#include <epicsString.h>
+#include <epicsTimer.h>
+#include <epicsMutex.h>
+#include <epicsEvent.h>
+#include <iocsh.h>
+
+#include "cpswAsynPortDriver.h"
+#include <cpsw_api_user.h>
+#include <epicsExport.h>
+
+#include <asynOctetSyncIO.h>
+
+
+static const char *driverName="cpswAsynDriver";
+static void asynPollerC(void *drvPvt);
+
+
+/** Constructor for the cpswAsynDriver class.
+  * Calls constructor for the asynPortDriver base class.
+  * \param[in] portName The name of the asyn port driver to be created.
+  * \param[in] maxPoints The maximum  number of points in the volt and time arrays */
+cpswAsynDriver::cpswAsynDriver(const char *portName, Path p, int nelms, int nEntries) 
+   : asynPortDriver(portName, 
+                    nelms, /* maxAddr */ 
+                    nEntries,
+                    asynInt32Mask | asynFloat64Mask | asynOctetMask | asynDrvUserMask, /* Interface mask */
+                    asynInt32Mask | asynFloat64Mask | asynOctetMask | asynEnumMask,  /* Interrupt mask */
+                    1, /* asynFlags.  This driver does not block and it is not multi-device, so flag is 0 */
+                    1, /* Autoconnect */
+                    0, /* Default priority */
+                    0), /* Default stack size*/   
+                    shuttingDown_(0) 
+{
+    asynStatus status;
+    int i;
+    const char *functionName = "cpswAsynDriver";
+
+    pollEventId_ = epicsEventMustCreate(epicsEventEmpty);
+    path_ = p;
+}
+
+/** Called when asyn clients call pasynInt32->write().
+  * This function sends a signal to the cyclicTask thread if the value of P_Run has changed.
+  * For all parameters it sets the value in the parameter library and calls any registered callbacks..
+  * \param[in] pasynUser pasynUser structure that encodes the reason and address.
+  * \param[in] value Value to write. */
+asynStatus cpswAsynDriver::writeInt32(asynUser *pasynUser, epicsInt32 value)
+{
+    int function = pasynUser->reason;
+    int addr=0;
+    asynStatus status = asynSuccess;
+    const char *paramName;
+    const char* functionName = "writeInt32";
+
+    ScalVal scalVal;
+    Path p;
+    char strPath[100];
+    uint32_t u32 = (uint32_t) value;
+    status = getAddress(pasynUser, &addr); if (status != asynSuccess) return(status);
+    
+    /* Fetch the parameter string name for possible use in debugging */
+    getParamName(function, &paramName);
+
+    sprintf(strPath, "%s[%d]/%s", portName, addr, paramName);
+    try {
+        p = path_->findByName( strPath );
+        scalVal = IScalVal::create( p );
+        scalVal->setVal( &u32, 1 );
+    } catch (CPSWError &e ) {
+        printf("CPSW Error: %s\n", e.getInfo().c_str());
+        status = asynError;
+    }
+    if (status != asynSuccess) return (status);
+
+    setIntegerParam(addr, function, value); 
+
+    /* Do callbacks so higher layers see any changes */
+    status = (asynStatus) callParamCallbacks();
+    
+    if (status) 
+        epicsSnprintf(pasynUser->errorMessage, pasynUser->errorMessageSize, 
+                  "%s:%s: status=%d, function=%d, name=%s, value=%d", 
+                  driverName, functionName, status, function, paramName, value);
+    else        
+        asynPrint(pasynUser, ASYN_TRACEIO_DRIVER, 
+              "%s:%s: function=%d, name=%s, value=%d\n", 
+              driverName, functionName, function, paramName, value);
+    return status;
+}
+
+/** Called when asyn clients call pasynFloat64Array->read().
+  * Returns the value of the P_Waveform or P_TimeBase arrays.  
+  * \param[in] pasynUser pasynUser structure that encodes the reason and address.
+  * \param[in] value Pointer to the array to read.
+  * \param[in] nElements Number of elements to read.
+  * \param[out] nIn Number of elements actually read. */
+asynStatus cpswAsynDriver::readInt32(asynUser *pasynUser, epicsInt32 *value)
+{
+    int function = pasynUser->reason;
+    int addr = 0;
+    asynStatus status = asynSuccess;
+    epicsTimeStamp timeStamp; getTimeStamp(&timeStamp);
+    const char *functionName = "readInt32";
+    const char *paramName;
+    
+    ScalVal scalVal;
+    Path p;
+    char strPath[100];
+
+    uint32_t u32;
+
+    status = getAddress(pasynUser, &addr); if (status != asynSuccess) return(status);
+
+    /* Fetch the parameter string name for possible use in debugging */
+    getParamName(function, &paramName);
+
+    sprintf(strPath, "%s[%d]/%s", portName, addr, paramName);
+    path_->dump( stdout ); fputc('\n', stdout);
+    //printf("%s:%s path: %s\n", driverName, functionName, strPath);
+    try { 
+        p = path_->findByName( strPath );
+        p->dump( stdout ); fputc('\n', stdout);
+        scalVal = IScalVal::create( p );
+        scalVal->getVal( &u32, 1 );
+    } catch (CPSWError &e ) {
+        printf("CPSW Error: %s\n", e.getInfo().c_str());
+        status = asynError;
+    }
+    if (status != asynSuccess) return (status);
+    *value = (epicsInt32) u32;
+    //printf("Path %s: value %ul", strPath, u32);
+    /* Set the timestamp */
+    setIntegerParam(addr, function, *value); 
+
+    /* Do callbacks so higher layers see any changes */
+    status = (asynStatus) callParamCallbacks();
+    pasynUser->timestamp = timeStamp;
+
+
+    if (status) 
+        epicsSnprintf(pasynUser->errorMessage, pasynUser->errorMessageSize, 
+                  "%s:%s: status=%d, function=%d", 
+                  driverName, functionName, status, function);
+    else        
+        asynPrint(pasynUser, ASYN_TRACEIO_DRIVER, 
+              "%s:%s: function=%d\n", 
+              driverName, functionName, function);
+    return status;
+}
+
+asynStatus cpswAsynDriver::readOctet(asynUser *pasynUser, char *value, size_t maxChars,
+                                        size_t *nActual, int *eomReason)
+{
+    int function = pasynUser->reason;
+    int addr = 0;
+    asynStatus status = asynSuccess;
+    epicsTimeStamp timeStamp; getTimeStamp(&timeStamp);
+    const char *functionName = "readOctet";
+    const char *paramName;
+    printf("ReadOctet\n"); 
+    ScalVal scalVal;
+    Path p;
+    char strPath[100];
+    
+//    status = getAddress(pasynUser, &addr); if (status != asynSuccess) return(status);
+    getAddress(pasynUser, &addr);
+    /* Fetch the parameter string name for possible use in debugging */
+    getParamName(function, &paramName);
+
+    sprintf(strPath, "%s[%d]/%s", portName, addr, paramName);
+    path_->dump( stdout ); fputc('\n', stdout);
+    printf("%s:%s path: %s\n", driverName, functionName, strPath);
+    try { 
+        p = path_->findByName( strPath );
+        scalVal = IScalVal::create( p );
+        *nActual = (size_t) scalVal->getVal( (uint8_t*)value, maxChars );
+    } catch (CPSWError &e ) {
+        printf("CPSW Error: %s\n", e.getInfo().c_str());
+        status = asynError;
+    }
+    if (status != asynSuccess) return (status);
+    /* Set the timestamp */
+    setStringParam(addr, function, value); 
+
+    /* Do callbacks so higher layers see any changes */
+    status = (asynStatus) callParamCallbacks();
+    pasynUser->timestamp = timeStamp;
+
+    if (status) 
+        epicsSnprintf(pasynUser->errorMessage, pasynUser->errorMessageSize, 
+                  "%s:%s: status=%d, function=%d", 
+                  driverName, functionName, status, function);
+    else        
+        asynPrint(pasynUser, ASYN_TRACEIO_DRIVER, 
+              "%s:%s: function=%d\n", 
+              driverName, functionName, function);
+    return status;
+}
+  
+/** Starts the poller thread.
+ ** Derived classes will typically call this at near the end of their constructor.
+ ** Derived classes can typically use the base class implementation of the poller thread,
+ ** but are free to reimplement it if necessary.
+ ** \param[in] pollPeriod The time between polls. */
+asynStatus cpswAsynDriver::startPoller(double pollPeriod)
+{
+  pollPeriod_ = pollPeriod;
+  epicsThreadCreate("asynPoller", 
+                    epicsThreadPriorityLow,
+                    epicsThreadGetStackSize(epicsThreadStackMedium),
+                    (EPICSTHREADFUNC)asynPollerC, (void *)this);                
+  return asynSuccess;
+}      
+
+/** Wakes up the poller thread to make it start polling. */
+asynStatus cpswAsynDriver::wakeupPoller()
+{ 
+  epicsEventSignal(pollEventId_);                                                                         
+  return asynSuccess;
+} 
+  
+/** Polls the device.
+ ** The base class cpswPoller thread calls this method once.
+ ** This base class implementation does nothing.  Derived classes can implement this method if there
+ ** are parameters that need to be polled. */
+asynStatus cpswAsynDriver::poll()    
+{                                                                        
+  return asynSuccess;
+} 
+  
+static void asynPollerC(void *drvPvt)
+{ 
+  cpswAsynDriver *pCpswDriver = (cpswAsynDriver*)drvPvt;      
+  pCpswDriver->asynPoller();                                                              
+} 
+
+void cpswAsynDriver::asynPoller()
+{
+  double timeout;
+  int status;
+
+  timeout = pollPeriod_;
+  wakeupPoller();  /* Force on poll at startup */
+
+  while(1) {
+    if (timeout != 0.) status = epicsEventWaitWithTimeout(pollEventId_, timeout);
+    else               status = epicsEventWait(pollEventId_);
+    if (status == epicsEventWaitOK) {
+ /* We got an event, rather than a timeout.  This is because other software
+ ** knows that we should do a poll.
+ **/
+    }
+    lock();
+    if (shuttingDown_) {
+      unlock();
+      break;
+    }
+    poll();
+    unlock();
+  }
+}
+
+
+/** Build a file name from component parts.
+  * \param[in] maxChars  The size of the fullFileName string.
+  * \param[out] fullFileName The constructed file name including the file path.
+  * 
+  * This is a convenience function that constructs a complete file name
+  * from the NDFilePath, NDFileName, NDFileNumber, and
+  * NDFileTemplate parameters. If NDAutoIncrement is true then it increments the
+  * NDFileNumber after creating the file name.
+  */
+int cpswAsynDriver::createFileName(int maxChars, char *fullFileName)
+{
+    /* Formats a complete file name from the components defined in NDStdDriverParams */
+    int status = asynSuccess;
+    char filePath[MAX_FILENAME_LEN];
+    char fileName[MAX_FILENAME_LEN];
+    char fileTemplate[MAX_FILENAME_LEN];
+    int fileNumber;
+    int autoIncrement;
+    int len;
+    
+    this->checkPath();
+    status |= getStringParam(NDFilePath, sizeof(filePath), filePath);
+    status |= getStringParam(NDFileName, sizeof(fileName), fileName); 
+    status |= getStringParam(NDFileTemplate, sizeof(fileTemplate), fileTemplate); 
+    status |= getIntegerParam(NDFileNumber, &fileNumber);
+    status |= getIntegerParam(NDAutoIncrement, &autoIncrement);
+    if (status) return(status);
+    len = epicsSnprintf(fullFileName, maxChars, fileTemplate, 
+                        filePath, fileName, fileNumber);
+    if (len < 0) {
+        status |= asynError;
+        return(status);
+    }
+    if (autoIncrement) {
+        fileNumber++;
+        status |= setIntegerParam(NDFileNumber, fileNumber);
+    }
+    return(status);   
+}
+
+/** Checks whether the directory specified NDFilePath parameter exists.
+  * 
+  * This is a convenience function that determinesthe directory specified NDFilePath parameter exists.
+  * It sets the value of NDFilePathExists to 0 (does not exist) or 1 (exists).  
+  * It also adds a trailing '/' character to the path if one is not present.
+  * Returns a error status if the directory does not exist.
+  */
+int cpswAsynDriver::checkPath()
+{
+    /* Formats a complete file name from the components defined in NDStdDriverParams */
+    int status = asynError;
+    char filePath[MAX_FILENAME_LEN];
+    int hasTerminator=0;
+    struct stat buff;
+    size_t len;
+    int isDir=0;
+    int pathExists=0;
+    
+    status = getStringParam(NDFilePath, sizeof(filePath), filePath);
+    len = strlen(filePath);
+    if (len == 0) return(asynSuccess);
+    /* If the path contains a trailing '/' or '\' remove it, because Windows won't find
+     * the directory if it has that trailing character */
+    if ((filePath[len-1] == '/') || (filePath[len-1] == '\\')) {
+        filePath[len-1] = 0;
+        len--;
+        hasTerminator=1;
+    }
+    status = stat(filePath, &buff);
+    if (!status) isDir = (S_IFDIR & buff.st_mode);
+    if (!status && isDir) {
+        pathExists = 1;
+        status = asynSuccess;
+    }
+    /* If the path did not have a trailing terminator then add it if there is room */
+    if (!hasTerminator) {
+        if (len < MAX_FILENAME_LEN-2) strcat(filePath, "/");
+        setStringParam(NDFilePath, filePath);
+    }
+    setIntegerParam(NDFilePathExists, pathExists);
+    return(status);   
+}
+/** Build a file name from component parts.
+  * \param[in] maxChars  The size of the fullFileName string.
+  * \param[out] filePath The file path.
+  * \param[out] fileName The constructed file name without file file path.
+  * 
+  * This is a convenience function that constructs a file path and file name
+  * from the NDFilePath, NDFileName, NDFileNumber, and
+  * NDFileTemplate parameters. If NDAutoIncrement is true then it increments the
+  * NDFileNumber after creating the file name.
+  */
+int cpswAsynDriver::createFileName(int maxChars, char *filePath, char *fileName)
+{
+    /* Formats a complete file name from the components defined in NDStdDriverParams */
+    int status = asynSuccess;
+    char fileTemplate[MAX_FILENAME_LEN];
+    char name[MAX_FILENAME_LEN];
+    int fileNumber;
+    int autoIncrement;
+    int len;
+    
+    this->checkPath();
+    status |= getStringParam(NDFilePath, maxChars, filePath); 
+    status |= getStringParam(NDFileName, sizeof(name), name); 
+    status |= getStringParam(NDFileTemplate, sizeof(fileTemplate), fileTemplate); 
+    status |= getIntegerParam(NDFileNumber, &fileNumber);
+    status |= getIntegerParam(NDAutoIncrement, &autoIncrement);
+    if (status) return(status);
+    len = epicsSnprintf(fileName, maxChars, fileTemplate, 
+                        name, fileNumber);
+    if (len < 0) {
+        status |= asynError;
+        return(status);
+    }
+    if (autoIncrement) {
+        fileNumber++;
+        status |= setIntegerParam(NDFileNumber, fileNumber);
+    }
+    return(status);   
+}
+
+extern "C" {
+
+/** EPICS iocsh callable function to call constructor for the cpswAsynDriver class.
+  * \param[in] portName The name of the asyn port driver to be created.
+  * \param[in] maxPoints The maximum  number of points in the volt and time arrays */
+int cpswAsynDriverConfigure(const char *portName, const char *path)
+{
+    Path p;
+    int nelms = 1;
+    int nentries = 0;
+
+    try {
+//        Path p = root->findByName( path );
+    } catch (CPSWError &e ) {
+        printf("CPSW Error: %s\n", e.getInfo().c_str());
+    }
+    new cpswAsynDriver(portName, p, nelms, nentries);
+    return(asynSuccess);
+}
+
+
+/* EPICS iocsh shell commands */
+
+static const iocshArg initArg0 = { "portName",iocshArgString};
+static const iocshArg initArg1 = { "path",iocshArgString};
+static const iocshArg * const initArgs[] = {&initArg0,
+                                            &initArg1};
+static const iocshFuncDef initFuncDef = {"cpswAsynDriverConfigure",2,initArgs};
+static void initCallFunc(const iocshArgBuf *args)
+{
+    cpswAsynDriverConfigure(args[0].sval, args[1].sval);
+}
+
+void cpswAsynDriverRegister(void)
+{
+    iocshRegister(&initFuncDef,initCallFunc);
+}
+
+epicsExportRegistrar(cpswAsynDriverRegister);
+
+}
+
---
+++ cpswAsynPortDriverApp/src/cpswAsynPortDriver.h
@@ -0,0 +1,119 @@
+#ifndef CPSW_ASYN_DRIVER_H
+#define CPSW_ASYN_DRIVER_H
+
+#include "asynPortDriver.h"
+#include <cpsw_api_user.h>
+
+#include <epicsEvent.h>
+#include <epicsTypes.h>
+
+/** Maximum length of a filename or any of its components */
+#define MAX_FILENAME_LEN 256
+
+
+#define NDFilePathString        "FILE_PATH"         /**< (asynOctet,    r/w) The file path */
+#define NDFilePathExistsString  "FILE_PATH_EXISTS"  /**< (asynInt32,    r/w) File path exists? */
+#define NDFileNameString        "FILE_NAME"         /**< (asynOctet,    r/w) The file name */
+#define NDFileNumberString      "FILE_NUMBER"       /**< (asynInt32,    r/w) The next file number */
+#define NDFileTemplateString    "FILE_TEMPLATE"     /**< (asynOctet,    r/w) The file format template; C-style format string */
+#define NDAutoIncrementString   "AUTO_INCREMENT"    /**< (asynInt32,    r/w) Autoincrement file number; 0=No, 1=Yes */
+#define NDFullFileNameString    "FULL_FILE_NAME"    /**< (asynOctet,    r/o) The actual complete file name for the last file saved */
+#define NDFileFormatString      "FILE_FORMAT"       /**< (asynInt32,    r/w) The data format to use for saving the file.  */
+#define NDAutoSaveString        "AUTO_SAVE"         /**< (asynInt32,    r/w) Automatically save files */
+#define NDWriteFileString       "WRITE_FILE"        /**< (asynInt32,    r/w) Manually save the most recent array to a file when value=1 */
+#define NDReadFileString        "READ_FILE"         /**< (asynInt32,    r/w) Manually read file when value=1 */
+#define NDFileWriteModeString   "WRITE_MODE"        /**< (asynInt32,    r/w) File saving mode (NDFileMode_t) */
+#define NDFileWriteStatusString "WRITE_STATUS"      /**< (asynInt32,    r/w) File write status */
+#define NDFileWriteMessageString "WRITE_MESSAGE"    /**< (asynOctet,    r/w) File write message */
+#define NDFileNumCaptureString  "NUM_CAPTURE"       /**< (asynInt32,    r/w) Number of arrays to capture */
+#define NDFileNumCapturedString "NUM_CAPTURED"      /**< (asynInt32,    r/o) Number of arrays already captured */
+#define NDFileCaptureString     "CAPTURE"           /**< (asynInt32,    r/w) Start or stop capturing arrays */
+#define NDFileDeleteDriverFileString  "DELETE_DRIVER_FILE"  /**< (asynInt32,    r/w) Delete driver file */
+#define NDFileLazyOpenString    "FILE_LAZY_OPEN"    /**< (asynInt32,    r/w) Don't open file until first frame arrives in Stream mode */
+
+
+/** Class that demonstrates the use of the asynPortDriver base class to greatly simplify the task
+  * of writing an asyn port driver.
+  * This class does a simple simulation of a digital oscilloscope.  It computes a waveform, computes
+  * statistics on the waveform, and does callbacks with the statistics and the waveform data itself. 
+  * I have made the methods of this class public in order to generate doxygen documentation for them,
+  * but they should really all be private. */
+class cpswAsynDriver : public asynPortDriver {
+public:
+    cpswAsynDriver(const char *portName, Path p, int nelms, int nEntries);
+                 
+    /* These are the methods that we override from asynPortDriver */
+    virtual asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
+    virtual asynStatus readInt32(asynUser *pasynUser, epicsInt32 *value);
+    virtual asynStatus readOctet(asynUser *pasynUser, char *value, size_t maxChars,
+                                        size_t *nActual, int *eomReason);
+    /* These are the methods that are new to this class */
+    void cyclicTask(void);
+    virtual asynStatus startPoller(double pollPeriod);
+    virtual asynStatus wakeupPoller();
+    virtual asynStatus poll();
+    void asynPoller();
+
+
+    /* These are the methods that are new to this class */
+    virtual int checkPath();
+    virtual int createFileName(int maxChars, char *fullFileName);
+    virtual int createFileName(int maxChars, char *filePath, char *fileName);
+protected:
+    /** cpsw path */
+    Path path_;
+
+    epicsEventId pollEventId_;    /**< Event ID to wake up poller */
+    double pollPeriod_;           /**< The time between polls */
+    int shuttingDown_;
+
+        int NDPortNameSelf;
+    #define FIRST_NDARRAY_PARAM NDPortNameSelf
+    int NDArraySizeX;
+    int NDArraySizeY;
+    int NDArraySizeZ;
+    int NDArraySize;
+    int NDNDimensions;
+    int NDDimensions;
+    int NDDataType;
+    int NDColorMode;
+    int NDUniqueId;
+    int NDTimeStamp;
+    int NDEpicsTSSec;
+    int NDEpicsTSNsec;
+    int NDBayerPattern;
+    int NDArrayCounter;
+    int NDFilePath;
+    int NDFilePathExists;
+    int NDFileName;
+    int NDFileNumber;
+    int NDFileTemplate;
+    int NDAutoIncrement;
+    int NDFullFileName;
+    int NDFileFormat;
+    int NDAutoSave;
+    int NDWriteFile;
+    int NDReadFile;
+    int NDFileWriteMode;
+    int NDFileWriteStatus;
+    int NDFileWriteMessage;
+    int NDFileNumCapture;
+    int NDFileNumCaptured;
+    int NDFileCapture;
+    int NDFileDeleteDriverFile;
+    int NDFileLazyOpen;
+    int NDAttributesFile;
+    int NDArrayData;
+    int NDArrayCallbacks;
+    int NDPoolMaxBuffers;
+    int NDPoolAllocBuffers;
+    int NDPoolFreeBuffers;
+    int NDPoolMaxMemory;
+    int NDPoolUsedMemory;
+    #define LAST_NDARRAY_PARAM NDPoolUsedMemory
+
+
+private:
+    /* Our data */
+};
+#endif
