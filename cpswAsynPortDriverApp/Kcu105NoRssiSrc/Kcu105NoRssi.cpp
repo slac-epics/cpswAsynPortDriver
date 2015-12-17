4bab243d21204097826eb55fccff3ac8c146004d
Mitch D'Ewart <mdewart@slac.stanford.edu>
2015-12-17 11:22:36 -0800

    Added Kcu105NoRssi.  Updated makefile for new directory structure

---
+++ cpswAsynPortDriverApp/Kcu105NoRssiSrc/Kcu105NoRssi.cpp
@@ -0,0 +1,73 @@
+#include <cpsw_mmio_dev.h>
+#include <string.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+#include <epicsExport.h>
+#include <iocsh.h>
+
+#include <cpsw_api_user.h>
+#include <cpsw_api_builder.h>
+
+#include "Kcu105NoRssi.h"
+
+
+#define AXIVERSION_BASE_ADDR_C   ( 0x00000000 )
+#define TENGIGETHREG_BASE_ADDR_C   ( 0x00020000 )
+#define PRBSTX_BASE_ADDR_C   ( 0x00030000 )
+
+Kcu105NoRssi IKcu105NoRssi::create(const char *name)
+{
+Kcu105NoRssiImpl v = CEntryImpl::create<Kcu105NoRssiImpl::element_type>(name);
+
+        AxiVersion p1 = IAxiVersion::create("AxiVersion");
+        v->CMMIODevImpl::addAtAddress( p1, AXIVERSION_BASE_ADDR_C );
+
+        TenGigEthReg p2 = ITenGigEthReg::create("TenGigEthReg");
+        v->CMMIODevImpl::addAtAddress( p2, TENGIGETHREG_BASE_ADDR_C );
+
+        PrbsTx p3 = IPrbsTx::create("PrbsTx");
+        v->CMMIODevImpl::addAtAddress( p3, PRBSTX_BASE_ADDR_C );
+
+        return v;
+}
+
+CKcu105NoRssiImpl::CKcu105NoRssiImpl(FKey key) : CMMIODevImpl(key, 0x000B0000 , LE)
+{
+}
+
+extern "C" int Kcu105NoRssiCreate(const char *ipAddr)
+{
+  
+  NoSsiDev r  = INoSsiDev::create("root", ipAddr);
+  Kcu105NoRssi Kcu105NoRssi = IKcu105NoRssi::create("Kcu105NoRssi");
+  Dev root = IDev::getRootDev();
+  r->addAtAddress( Kcu105NoRssi, INoSsiDev::SRP_UDP_V2, 8192 );
+  root->addAtAddress( r );
+
+  return 1;
+}
+
+
+/** Code for iocsh registration */
+static const iocshArg Kcu105NoRssiCreateArg0 = {"IP Address", iocshArgString};
+static const iocshArg * const Kcu105NoRssiCreateArgs[] = {&Kcu105NoRssiCreateArg0};
+static const iocshFuncDef Kcu105NoRssiCreateDef = {"Kcu105NoRssiCreate", 1, Kcu105NoRssiCreateArgs};
+static void Kcu105NoRssiCreateCallFunc(const iocshArgBuf *args)
+{
+  Kcu105NoRssiCreate(args[0].sval);
+}
+
+static void Kcu105NoRssiRegister(void)
+{
+  iocshRegister(&Kcu105NoRssiCreateDef, Kcu105NoRssiCreateCallFunc);
+}
+
+extern "C" {
+epicsExportRegistrar(Kcu105NoRssiRegister);
+}
+
+
---
+++ cpswAsynPortDriverApp/Kcu105NoRssiSrc/Kcu105NoRssi.h
@@ -0,0 +1,25 @@
+#ifndef KCU105NORSSI_DRIVER_H
+#define KCU105NORSSI_DRIVER_H
+
+#include <cpsw_api_builder.h>
+#include <cpsw_mmio_dev.h>
+#include <device_library.h>
+
+class   IKcu105NoRssi;
+typedef shared_ptr<IKcu105NoRssi> Kcu105NoRssi;
+
+class CKcu105NoRssiImpl;
+typedef shared_ptr<CKcu105NoRssiImpl> Kcu105NoRssiImpl;
+
+
+class IKcu105NoRssi : public virtual IMMIODev {
+public:
+        static Kcu105NoRssi create(const char *name);
+};
+
+class CKcu105NoRssiImpl : public CMMIODevImpl, public virtual IKcu105NoRssi {
+public:
+        CKcu105NoRssiImpl(FKey);
+};
+#endif
+
