a47ba9d182a9f7e55586fda8935fe9b5ab9f0212
Mitch D'Ewart <mdewart@slac.stanford.edu>
2015-12-18 10:07:59 -0800

    Added iocKcu105NoRssi generated st.cmd

--- iocBoot/iocKcu105NoRssi/st.cmd
+++ iocBoot/iocKcu105NoRssi/st.cmd
@@ -4,25 +4,20 @@
 
 cd ${TOP}
 
-## Register all support components
 dbLoadDatabase("dbd/Kcu105NoRssiApp.dbd")
 Kcu105NoRssiApp_registerRecordDeviceDriver(pdbbase)
 
 #create a Kcu105NoRssi
-#Kc705TenGigEthCreate("192.168.2.10")
 Kcu105NoRssiCreate("192.168.2.10")
 
 
-#find AxiVersion at address root/Kcu105NoRssi
-AxiVersionCreate("AxiVersion", "root/Kcu105NoRssi")
-TenGigEthRegCreate("TenGigEthReg", "root/Kcu105NoRssi")
-PrbsTxCreate("PrbsTx", "root/Kcu105NoRssi")
+AxiVersionCreate("AxiVersion", "root/Kcu105NoRssi" )
+TenGigEthRegCreate("TenGigEthReg", "root/Kcu105NoRssi" )
+PrbsTxCreate("PrbsTx", "root/Kcu105NoRssi" )
 
 dbLoadRecords("db/AxiVersion.db")
-#dbLoadRecords("db/AxiXadc.db")
 dbLoadRecords("db/TenGigEthReg.db")
 dbLoadRecords("db/PrbsTx.db")
 
 iocInit()
-#dbl()
-#asynReport 11
+
